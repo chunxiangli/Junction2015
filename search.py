00b0a6682a3a4b488bd60bfdaefad42deb13d9d6
Shen Huibin <huibin@Shens-MacBook-Pro.local>
2015-11-07 13:02:53 +0200

    interface defined

--- search.py
+++ search.py
@@ -1,15 +1,15 @@
-
+from worker import findThem 
 
 def search(citys, startTime, endTime):
     """
     Parameters:
     -----------
-    homecity: "Helsinki"
+    homeCity: "Helsinki"
     citys: ["Paris", "Rome"]
     startTime:  "2015-11-01"
     endTime: "2015-11-17"
 
-    Returns:
+    Returns: JSON
     --------
     {
       "options": 
@@ -19,6 +19,7 @@ def search(citys, startTime, endTime):
                      {"startCity": "Rome", "endCity": "Paris", "startTime": "2015-11-04 14:30", "endTime": "2015-11-04 16:00", "price": 189.10},
                      {"startCity": "Paris", "endCity": "Helsinki", "startTime": "2015-11-07 20:30", "endTime": "2015-11-07 22:00", "price": 170.80}]
         },
+
         { "totalPrice": 610.80, 
           "trips": [ {"startCity": "Helsinki", "endCity": "Paris", "startTime": "2015-11-01 08:30", "endTime": "2015-11-01 10:30", "price": 250.90},
                      {"startCity": "Paris", "endCity": "Rome", "startTime": "2015-11-04 14:30", "endTime": "2015-11-04 16:00", "price": 189.10},
@@ -26,6 +27,7 @@ def search(citys, startTime, endTime):
         }        
       ]
     }
-    
+        
     """
-    
+
+    return findThem(homeCity, citys, startTime, endTime)
