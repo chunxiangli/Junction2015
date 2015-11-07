0bae4902df10c3712cb8ace70001806bf74a6a5c
Shen Huibin <huibin@Shens-MacBook-Pro.local>
2015-11-07 12:49:49 +0200

    interface

---
+++ search.py
@@ -0,0 +1,31 @@
+
+
+def search(citys, startTime, endTime):
+    """
+    Parameters:
+    -----------
+    homecity: "Helsinki"
+    citys: ["Paris", "Rome"]
+    startTime:  "2015-11-01"
+    endTime: "2015-11-17"
+
+    Returns:
+    --------
+    {
+      "options": 
+      [ 
+        { "totalPrice": 560.80, 
+          "trips": [ {"startCity": "Helsinki", "endCity": "Rome", "startTime": "2015-11-01 08:30", "endTime": "2015-11-01 10:30", "price": 200.90},
+                     {"startCity": "Rome", "endCity": "Paris", "startTime": "2015-11-04 14:30", "endTime": "2015-11-04 16:00", "price": 189.10},
+                     {"startCity": "Paris", "endCity": "Helsinki", "startTime": "2015-11-07 20:30", "endTime": "2015-11-07 22:00", "price": 170.80}]
+        },
+        { "totalPrice": 610.80, 
+          "trips": [ {"startCity": "Helsinki", "endCity": "Paris", "startTime": "2015-11-01 08:30", "endTime": "2015-11-01 10:30", "price": 250.90},
+                     {"startCity": "Paris", "endCity": "Rome", "startTime": "2015-11-04 14:30", "endTime": "2015-11-04 16:00", "price": 189.10},
+                     {"startCity": "Rome", "endCity": "Helsinki", "startTime": "2015-11-07 20:30", "endTime": "2015-11-07 22:00", "price": 170.80}]
+        }        
+      ]
+    }
+    
+    """
+    
